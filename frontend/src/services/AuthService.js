df54a0115e0766635db083f51d6f7d6dd216f9e7
marcoavfcc01121979 <marcoavfcc@gmail.com>
2023-06-16 15:10:16 -0300

    fluxo de login concluido no front

---
+++ frontend/src/services/AuthService.js
@@ -0,0 +1,10 @@
+export function doLogin(email, password) {
+  return new Promise((response, reject) => {
+    if (email === "admin@admin.com" && password === "123456") {
+      response(true);
+    }
+    reject(`Invalid user and/or password!`);
+  });
+}
+
+export function doLogout() {}
