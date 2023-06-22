19d3c252334287d23de4565d0677e15863437200
marcoavfcc01121979 <marcoavfcc@gmail.com>
2023-06-21 21:43:41 -0300

    banco de dados finalizado

---
+++ backend/seeders/20230621011248-default-settings.js
@@ -0,0 +1,35 @@
+"use strict";
+require("dotenv").config();
+const bcrypt = require("bcryptjs");
+const crypto = require("../src/utils/crypto");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const settingsId = await queryInterface.rawSelect(
+      "Settings",
+      { where: {}, limit: 1 },
+      ["id"]
+    );
+    if (!settingsId) {
+      return queryInterface.bulkInsert("settings", [
+        {
+          email: "admin@admin.com",
+          password: bcrypt.hashSync("123456"),
+          apiUrl: "https://testnet.binance.vision/api/",
+          accessKey:
+            "RyZOon5ks56CHXBZNMTIKdl7mKRVNpzGL7zmj9Rg63A4bQiasKFn03aXVatnChH8",
+          secretKey: crypto.encrypt(
+            "2wRSKaoB5rjoso0hsz527kZT9GwgG1HtIUQM0jEm4XIthXsDeUIyp6kvffhlMP5q"
+          ),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ]);
+    }
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("settings", null, {});
+  },
+};
--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,23 +1,22 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const settingsRepository = require("../repositories/settingsRepository");
 
-function doLogin(req, res, next) {
+async function doLogin(req, res, next) {
   const { email, password } = req.body;
 
-  if (
-    email === "admin@admin.com" &&
-    bcrypt.compareSync(
-      password,
-      "$2a$12$8NvTNdAunk6.tpLCytbA6uiKiwyDxQc.aj6bnLaTwtqSqrhALttDy"
-    )
-  ) {
-    const token = jwt.sign({ id: 1 }, process.env.JWT_SECRET, {
-      expiresIn: parseInt(process.env.JWT_EXPIRES),
-    });
-    res.json({ token });
-  } else {
-    res.sendStatus(401);
+  const settings = await settingsRepository.getSettingsByEmail(email);
+
+  if (settings) {
+    const isValid = bcrypt.compareSync(password, settings.password);
+    if (isValid) {
+      const token = jwt.sign({ id: settings.id }, process.env.JWT_SECRET, {
+        expiresIn: parseInt(process.env.JWT_EXPIRES),
+      });
+      return res.json({ token });
+    }
   }
+  res.sendStatus(401);
 }
 
 const blacklist = [];
